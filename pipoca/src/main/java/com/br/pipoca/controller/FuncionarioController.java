17cc2a28e695d2e5faaa89e2e39919f1b612ddf1
Matheus Barbosa <ps.matheusb@gmail.com>
2022-06-02 19:51:25 -0300

    update funcionario

--- pipoca/src/main/java/com/br/pipoca/controller/FuncionarioController.java
+++ pipoca/src/main/java/com/br/pipoca/controller/FuncionarioController.java
@@ -1,2 +1,29 @@
-package com.br.pipoca.controller;public class FuncionarioController {
+package com.br.pipoca.controller;
+
+import com.br.pipoca.entity.Funcionario;
+import com.br.pipoca.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/funcionario")
+public class FuncionarioController {
+
+    @Autowired
+    FuncionarioRepository repository;
+
+    @GetMapping
+    public List<Funcionario> list() throws IOException{
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public void salvar (@RequestBody Funcionario funcionario){
+        System.out.println("salvando funcionario " + funcionario.getNome());
+        repository.save(funcionario);
+    }
+
 }
--- pipoca/src/main/java/com/br/pipoca/entity/Funcionario.java
+++ pipoca/src/main/java/com/br/pipoca/entity/Funcionario.java
@@ -2,11 +2,11 @@ package com.br.pipoca.entity;
 
 
 import com.br.pipoca.model.Sexo;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
 @Entity
@@ -22,7 +22,9 @@ public class Funcionario {
     private Date dtNascimento;
     private String funcao;
 
-    public Funcionario(long id) {
+    public Funcionario(){}
+
+    public Funcionario(Long id) {
         this.id = id;
     }
 
