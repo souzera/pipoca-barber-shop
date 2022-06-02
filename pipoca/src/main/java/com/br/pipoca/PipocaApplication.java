4445685739eec32c98c1ccd78e0e2f1bc15addb4
Matheus Barbosa <ps.matheusb@gmail.com>
2022-06-02 09:51:55 -0300

    initial commit

---
+++ pipoca/src/main/java/com/br/pipoca/PipocaApplication.java
@@ -0,0 +1,13 @@
+package com.br.pipoca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipocaApplication.class, args);
+	}
+
+}
---
+++ pipoca/src/test/java/com/br/pipoca/PipocaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pipoca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
