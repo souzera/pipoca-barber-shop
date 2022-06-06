d1e19c9ef3cd70e1ea283ce0e0e1afebfab8dc3a
Matheus Barbosa <ps.matheusb@gmail.com>
2022-06-06 11:33:02 -0300

    update Horario Rest controller

--- pipoca/src/main/java/com/br/pipoca/controller/HorarioController.java
+++ pipoca/src/main/java/com/br/pipoca/controller/HorarioController.java
@@ -1,29 +1,54 @@
 package com.br.pipoca.controller;
 
 import com.br.pipoca.entity.Horario;
+import com.br.pipoca.entity.Servico;
 import com.br.pipoca.repository.HorarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/horario")
+@RequestMapping(value = "/api")
 public class HorarioController {
 
     @Autowired
     HorarioRepository repository;
 
-    @GetMapping
+
+    @GetMapping(value = "/horarios")
     public List<Horario> list() throws IOException {
         return repository.findAll();
     }
 
-    @PostMapping
-    public void salvar (@RequestBody Horario horario){
-        System.out.println("salvando horario " + horario.getData());
+    @GetMapping(value = "/horario/{id}")
+    public Horario buscarHorario(@RequestParam(value = "id") long id){
+        return repository.findById(id);
+    }
+
+    @PostMapping(value = "/horario/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void salvar(@RequestBody Horario horario){
+        System.out.println("salvando serviço " + horario.getData());
         repository.save(horario);
+    }
+
+    @DeleteMapping(value = "/horario/del")
+    public void deletar(@RequestBody Horario horario){
+        repository.delete(horario);
+    }
+
+    @DeleteMapping(value = "/horario/del/{id}")
+    public void deletarPorID(@PathVariable(value = "id") long id){
+        repository.deleteById(id);
+    }
+
+    @PutMapping(value = "/horario/att")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Horario atualizar(@RequestBody Horario horario){
+        return repository.save(horario);
     }
 
 }
