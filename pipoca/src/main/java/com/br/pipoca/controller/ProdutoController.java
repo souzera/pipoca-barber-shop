a5dc868e03f2ed2755c719fbeb97cc5689b8c8a4
Matheus Barbosa <ps.matheusb@gmail.com>
2022-06-06 11:33:42 -0300

    update Produto Rest Controller

--- pipoca/src/main/java/com/br/pipoca/controller/ProdutoController.java
+++ pipoca/src/main/java/com/br/pipoca/controller/ProdutoController.java
@@ -1,29 +1,53 @@
 package com.br.pipoca.controller;
 
+import com.br.pipoca.entity.Horario;
 import com.br.pipoca.entity.Produto;
 import com.br.pipoca.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/produto")
+@RequestMapping(value = "/api")
 public class ProdutoController {
 
 
     @Autowired
     ProdutoRepository repository;
 
-    @GetMapping
+    @GetMapping(value = "/produtos")
     public List<Produto> list() throws IOException {
         return repository.findAll();
     }
 
-    @PostMapping
-    public void salvar (@RequestBody Produto produto){
-        System.out.println("salvando produto " + produto.getDescricao());
+    @GetMapping(value = "/produto/{id}")
+    public Produto buscarProduto(@RequestParam(value = "id") long id){
+        return repository.findById(id);
+    }
+
+    @PostMapping(value = "/produto/add")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void salvar(@RequestBody Produto produto){
+        System.out.println("salvando serviço " + produto);
         repository.save(produto);
+    }
+
+    @DeleteMapping(value = "/produto/del")
+    public void deletar(@RequestBody Produto produto){
+        repository.delete(produto);
+    }
+
+    @DeleteMapping(value = "/produto/del/{id}")
+    public void deletarPorID(@PathVariable(value = "id") long id){
+        repository.deleteById(id);
+    }
+
+    @PutMapping(value = "/produto/att")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Produto atualizar(@RequestBody Produto produto){
+        return repository.save(produto);
     }
 }
